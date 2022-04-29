2197d92055c97cd94b6b5859dae7f502e2af41ed
Michael Ugochukwu <mark.omulu@gmail.com>
2022-04-29 14:20:16 +0100

    Add clear all method

--- src/remove-task.js
+++ src/remove-task.js
@@ -9,4 +9,12 @@ export default class Delete {
   }
 
   // Delete all completed tasks
+  static deleteAll(arr) {
+    arr = arr.filter((element) => element.completed !== 'true');
+    for (let i = 0; i < arr.length; i += 1) {
+      arr[i].index = i + 1;
+    }
+    setStorage(arr);
+    return arr;
+  }
 }
